30f5152e8282ee2b6d6c7046599fbfd51607c664
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-04-18 02:31:51 -0400

    updated

--- controllers/api/comment-route.js
+++ controllers/api/comment-route.js
@@ -1,7 +1,55 @@
 const router = require('express').Router
-const {Post, Comment, Users} = require('../../models')
+const {Post, Comment, Users, User} = require('../../models')
+
+router.get('/', (req, res) => {
+
+    Comment.findAll ()
+    
+    .then(data => res.json(data))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+//create a comment
+
+router.post('/', (req,res) =>{
+
+Comment.create(
+    {
+        comment_text: req.body.comment_text,
+        user_id: req.session.ser_id,
+        post_id: req.body.post_id
+    }
+)
+.then(data => res.json(data))
+.catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+})
+
+})
 
 
+//delete comment
 
+router.delete('/', (req,res) => {
 
-module.exports =router
+    Comment.destroy({
+        where: {id: req.params.id}
+    })
+    .then(data => {
+        if (!data) {
+            res.status(404).json({ message: 'no comment found' })
+            return
+        } res.json(data)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+
+})
+
+module.exports = router
